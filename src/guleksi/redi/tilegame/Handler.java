67ae783cc6c04704f31f0770a3b56ee58d3055af
RediG7 <guleksiredi7@gmail.com>
2021-03-08 21:27:46 +0100

    Add Handler

---
+++ src/guleksi/redi/tilegame/Handler.java
@@ -0,0 +1,47 @@
+package guleksi.redi.tilegame;
+
+import guleksi.redi.tilegame.gfx.GameCamera;
+import guleksi.redi.tilegame.input.KeyManager;
+import guleksi.redi.tilegame.worlds.World;
+
+public class Handler {
+
+    private Game game;
+    private World world;
+
+    public Handler(Game game) {
+        this.game = game;
+    }
+
+    public GameCamera getGameCamera() {
+        return game.getGameCamera();
+    }
+
+    public KeyManager getKeyManager() {
+        return game.getKeyManager();
+    }
+
+    public int getWidth() {
+        return game.getWidth();
+    }
+
+    public int getHeight() {
+        return game.getHeight();
+    }
+
+    public Game getGame() {
+        return game;
+    }
+
+    public void setGame(Game game) {
+        this.game = game;
+    }
+
+    public World getWorld() {
+        return world;
+    }
+
+    public void setWorld(World world) {
+        this.world = world;
+    }
+}
