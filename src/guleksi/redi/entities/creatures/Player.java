ca18d4517f04a1b55aaa0756e306050ba6a18177
RediG7 <guleksiredi7@gmail.com>
2021-03-08 18:15:13 +0100

    Update Movement, Create Getters Setters

--- src/guleksi/redi/entities/creatures/Player.java
+++ src/guleksi/redi/entities/creatures/Player.java
@@ -1,23 +1,46 @@
 package guleksi.redi.entities.creatures;
 
 import guleksi.redi.gfx.Assets;
+import guleksi.redi.tilegame.Game;
 
 import java.awt.*;
 
-public class Player extends Creature{
+public class Player extends Creature {
 
-    public Player(float x, float y) {
-        super(x, y);
+    private Game game;
+
+    public Player(Game game, float x, float y) {
+        super(x, y, Creature.DEFAULT_CREATURE_WIDTH, Creature.DEFAULT_CREATURE_HEIGHT);
+        this.game = game;
     }
 
     @Override
     public void tick() {
+        getInput();
+        move();
+    }
 
+    private void getInput() {
+        xMove = 0;
+        yMove = 0;
+
+        if (game.getKeyManager().up) {
+            yMove = -speed;
+        }
+        if (game.getKeyManager().down) {
+            yMove = speed;
+        }
+        if (game.getKeyManager().left) {
+            xMove = -speed;
+        }
+        if (game.getKeyManager().right) {
+            xMove = speed;
+        }
     }
 
     @Override
     public void render(Graphics g) {
-        g.drawImage(Assets.player1, (int)x, (int)y, null);
+        g.drawImage(Assets.player1, (int) x, (int) y, width, height, null);
     }
 
 }
