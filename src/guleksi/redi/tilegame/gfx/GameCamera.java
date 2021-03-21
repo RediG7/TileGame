8bbc069b9bb8cae0d1ddba1b1f4b22947eded21d
RediG7 <guleksiredi7@gmail.com>
2021-03-21 13:15:53 +0100

    Add Camera Limits

--- src/guleksi/redi/tilegame/gfx/GameCamera.java
+++ src/guleksi/redi/tilegame/gfx/GameCamera.java
@@ -1,27 +1,44 @@
 package guleksi.redi.tilegame.gfx;
 
 import guleksi.redi.tilegame.entities.Entity;
-import guleksi.redi.tilegame.Game;
+import guleksi.redi.tilegame.Handler;
+import guleksi.redi.tilegame.tiles.Tile;
 
 public class GameCamera {
 
-    private Game game;
+    private Handler handler;
     private float xOffset, yOffset;
 
-    public GameCamera(Game game, float xOffset, float yOffset) {
-        this.game = game;
+    public GameCamera(Handler handler, float xOffset, float yOffset) {
+        this.handler = handler;
         this.xOffset = xOffset;
         this.yOffset = yOffset;
     }
 
+    // check if camer is showing blank spaces
+    public void checkBlankSpace() {
+        if (xOffset < 0) {
+            xOffset = 0;      // size of our world in pixels                    window width
+        } else if (xOffset > handler.getWorld().getWidth() * Tile.TILE_WIDTH - handler.getWidth()) {
+            xOffset = handler.getWorld().getWidth() * Tile.TILE_WIDTH - handler.getWidth();
+        }
+        if (yOffset < 0){
+            yOffset = 0;
+        } else if (yOffset > handler.getWorld().getHeight() * Tile.TILE_HEIGHT - handler.getHeight()) {
+            yOffset = handler.getWorld().getHeight() * Tile.TILE_HEIGHT - handler.getHeight();
+        }
+    }
+
     public void centerOnEntity(Entity e) {
-        xOffset = e.getX() - game.getWidth() / 2 + e.getWidth() / 2;
-        yOffset = e.getY() - game.getHeight() / 2 + e.getHeight() / 2;
+        xOffset = e.getX() - handler.getWidth() / 2 + e.getWidth() / 2;
+        yOffset = e.getY() - handler.getHeight() / 2 + e.getHeight() / 2;
+        checkBlankSpace();
     }
 
     public void move(float xAmount, float yAmount) {
         xOffset += xAmount;
         yOffset += yAmount;
+        checkBlankSpace();
     }
 
     public float getxOffset() {
