9f74f95052fda9b9c2293dd5022714c91f30f785
RediG7 <guleksiredi7@gmail.com>
2021-03-21 17:37:05 +0100

    Sort render order

--- src/guleksi/redi/tilegame/entities/EntityManager.java
+++ src/guleksi/redi/tilegame/entities/EntityManager.java
@@ -5,12 +5,23 @@ import guleksi.redi.tilegame.entities.creatures.Player;
 
 import java.awt.*;
 import java.util.ArrayList;
+import java.util.Comparator;
 
 public class EntityManager {
 
     private Handler handler;
     private Player player;
     private ArrayList<Entity> entities;
+    private Comparator<Entity> renderSorter = new Comparator<Entity>() {
+        @Override
+        public int compare(Entity a, Entity b) {
+            // render from top to bottom based on Y coordinate, but we need
+            // to check the bottom y-coordinate of the entities ( y coordinate + height )
+            if (a.getY() + a.getHeight() < b.getY() + b.getHeight())
+                return -1; // a should be rendered before b
+            return 1; // a should be rendered after b (if a.getY() > b.getY())
+        }
+    };
 
     public EntityManager(Handler handler, Player player) {
         this.handler = handler;
@@ -24,6 +35,7 @@ public class EntityManager {
             Entity e = entities.get(i);
             e.tick();
         }
+        entities.sort(renderSorter);
     }
 
     public void render(Graphics g) {
