fc9ab4ab10fc8d6814bce282de73b08c8e21ee8d
RediG7 <guleksiredi7@gmail.com>
2021-03-22 14:28:20 +0100

    Basic Menu State

--- src/guleksi/redi/tilegame/states/MenuState.java
+++ src/guleksi/redi/tilegame/states/MenuState.java
@@ -1,22 +1,38 @@
 package guleksi.redi.tilegame.states;
 
 import guleksi.redi.tilegame.Handler;
+import guleksi.redi.tilegame.gfx.Assets;
+import guleksi.redi.tilegame.ui.ClickListener;
+import guleksi.redi.tilegame.ui.UIImageButton;
+import guleksi.redi.tilegame.ui.UIManager;
 
 import java.awt.*;
 
-public class MenuState extends State{
+public class MenuState extends State {
+
+    private UIManager uiManager;
 
     public MenuState(Handler handler) {
         super(handler);
+        uiManager = new UIManager(handler);
+        handler.getMouseManager().setUIManager(uiManager);
+
+        uiManager.addObject(new UIImageButton(200, 200, 128, 64, Assets.btn_start, new ClickListener() {
+            @Override
+            public void onClick() {
+                handler.getMouseManager().setUIManager(null); // dont forget
+                State.setState(handler.getGame().gameState);
+            }
+        }));
     }
 
     @Override
     public void tick() {
-
+        uiManager.tick();
     }
 
     @Override
     public void render(Graphics g) {
-
+        uiManager.render(g);
     }
 }
